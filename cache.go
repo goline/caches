274634e1d1ff8f3b45031412884d864e40806073
Long Do <me@dotronglong.com>
2017-10-06 08:06:44 +0700

    Add New for cache

--- cache.go
+++ cache.go
@@ -1,9 +1,25 @@
 package caches
 
+import "time"
+
 type Cache interface {
 	// Get returns an appropriate cache item
 	Get(id string) CacheItem
 
-	// Set puts item into cache
-	Set(item CacheItem) Cache
+	// Set adds values into cache
+	Set(id string, value interface{}, expiration time.Duration) Cache
+
+	// Put puts item into cache
+	Put(item CacheItem) Cache
+}
+
+// New returns a cache instance by connection url
+// Example:
+// 		caches.New("redis://user:password@localhost:6379/0")
+// It will return Redis cache instance with options:
+// 		user=user password=password
+//		host=localhost port=6379
+//		database=0 (default database)
+func New(url string) Cache {
+	return nil
 }
