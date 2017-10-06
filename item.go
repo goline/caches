f3d937ef13cfc7ccc5675bfec824bc2016f721db
Long Do <me@dotronglong.com>
2017-10-06 07:59:03 +0700

    Update interfaces

--- item.go
+++ item.go
@@ -6,12 +6,23 @@ type CacheItem interface {
 	// IsHit returns true if cache is hit, and false if miss
 	IsHit() bool
 
-	// Value gets value into input argument
+	// Value allows to get an object from cache
 	Value(v interface{}) error
 
-	// WithValue sets input value into cache
+	// WithValue sets an object into cache
 	WithValue(v interface{}) CacheItem
 
 	// Expire sets expiration for item
 	Expire(t time.Duration) CacheItem
+}
+
+type CacheItemGetter interface {
+	// String returns value as string
+	String() (string, error)
+
+	// Int returns value as int64
+	Int() (int64, error)
+
+	// Float returns value as float64
+	Float() (float64, error)
 }
